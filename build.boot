af06a8c0d5a22e7afa635e258cfa348516c0ff1f
Zenathark <jcgalanh@gmail.com>
2017-11-14 00:06:26 -0600

    Add matrix color type

--- build.boot
+++ build.boot
@@ -2,4 +2,5 @@
  :source-paths #{"src/clj"}
  :dependencies '[[org.clojure/clojure "1.9.0-beta3"]
                  [net.mikera/core.matrix "0.61.0"]
-                 [net.mikera/vectorz-clj "0.47.0"]])
+                 [net.mikera/vectorz-clj "0.47.0"]
+                 [net.mikera/imagez "0.12.0"]])
--- src/clj/zenathark/wavelets/core.clj
+++ src/clj/zenathark/wavelets/core.clj
@@ -1,7 +1,35 @@
 (ns zenathark.wavelets.core
-  (:require [clojure.core.matrix :as m]))
+  (:gen-class)
+  (:require [clojure.core.matrix :as m]
+            [mikera.image.core :as mik])
+  (:import [java.awt.image BufferedImage]))
 
 (defn tens []
   (println "Hello, Clojure!"))
 
-(def a [[1 2] [3 4]])
+(defrecord RGB [red green blue])
+(defrecord Gray [gray])
+
+(defn convert
+  [tp color]
+  (case tp
+    :Gray (Gray. (+ (* 0.299 (:red color))
+                    (* 0.587 (:green color))
+                    (* 0.114 (:blue color))))))
+
+(defn image->color-matrix
+  [^BufferedImage image]
+  (let [pixels (mik/get-pixels image)]
+    (mapv (fn [i]
+            (map (fn [j]
+                   (let [pixel (bit-and 0xFFFFFFFF (nth pixels i j))
+                         red   (bit-shift-right (bit-and 0x00FF0000 pixel) 16)
+                         green (bit-shift-right (bit-and 0x0000FF00 pixel) 8)
+                         blue  (bit-and 0x000000FF pixel)]
+                     (RGB. red green blue)))
+                 (range (mik/width image))))
+          (range (mik/height image) ))))
+
+(defn convert-image
+  [tp image]
+  (mapv #(mapv (fn [pixel] (convert tp pixel)) %) image))
