5f975cf5ad626addeea6c90664ddc606227f0144
Zenathark <jcgalanh@gmail.com>
2017-11-15 23:20:24 -0600

    Add dynamic routes to server FIXME imshow

--- project.clj
+++ project.clj
@@ -2,23 +2,24 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "MIT License"
-            :url "https://tldrlegal.com/license/mit-license"}
-  :repositories [["java.net" "https://download.java.net/maven/2"]
-                 ["sonatype" {:url "https://oss.sonatype.org/content/repositories/releases"
+            :url  "https://tldrlegal.com/license/mit-license"}
+  :repositories [["clojars.org" "https://clojars.org/repo"]
+                 ["java.net" "https://download.java.net/maven/2"]
+                 ["sonatype" {:url           "https://oss.sonatype.org/content/repositories/releases"
                               ;; If a repository contains releases only setting
                               ;; :snapshots to false will speed up dependencies.
-                              :snapshots false
+                              :snapshots     false
                               ;; Disable signing releases deployed to this repo.
                               ;; (Not recommended.)
                               :sign-releases false
                               ;; You can also set the policies for how to handle
                               ;; :checksum failures to :fail, :warn, or :ignore.
-                              :checksum :fail
+                              :checksum      :fail
                               ;; How often should this repository be checked for
                               ;; snapshot updates? (:daily, :always, or :never)
-                              :update :always
+                              :update        :always
                               ;; You can also apply them to releases only:
-                              :releases {:checksum :fail :update :always}}]
+                              :releases      {:checksum :fail :update :always}}]
                  ["nuxeo" "https://maven-eu.nuxeo.org/nexus/content/repositories/public-releases/"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/imagez "0.12.0"]
@@ -29,14 +30,14 @@
   :java-source-paths ["src/main/java"]
   :resource-paths ["src/main/resources"]
   :test-paths ["src/test/clj"]
-  :profiles {:dev {:resource-paths ["src/test/resources"]
-                   :dependencies [[clj-stacktrace "0.2.8"]
-                                  [javax.servlet/servlet-api "2.5"]
-                                  [ring-mock "0.1.5"]]}
+  :profiles {:dev     {:resource-paths ["src/test/resources"]
+                       :dependencies   [[clj-stacktrace "0.2.8"]
+                                        [javax.servlet/servlet-api "2.5"]
+                                        [ring-mock "0.1.5"]]}
              :uberjar {:aut :all}}
   :main net.zenathark.wavelets.core
   :plugins [[lein-ring "0.12.1"]]
   :ring {:handler net.zenathark.wavelets.app-handler/app
-         :nrepl {:start? true
-                 :port 8080}}
+         :nrepl   {:start?  true
+                   :port    8080}}
   :target-path "target/%s")
--- src/main/clj/net/zenathark/wavelets/app_handler.clj
+++ src/main/clj/net/zenathark/wavelets/app_handler.clj
@@ -1,32 +1,50 @@
 (ns net.zenathark.wavelets.app-handler
   (:gen-class)
-  (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]
+            [compojure.route :as croute]
             [net.zenathark.wavelets.image :as zimage]
-            [ring.util.response :refer [response content-type]]))
-
-(defn- hello-answer []
-  "Hello")
+            [ring.util.response :refer [response content-type]]
+            [compojure.core :as ccore]))
 
 (defroutes app
   (GET "/test" [] (hello-answer)))
 
-(defroutes app-routes
-  (route/not-found
-   (response "Not Found")))
-
-(defn wrap-log-request [handler]
-  (fn [req]
-    (println req)
-    (handler req)))
-
 (defn response-image [image fmt]
   (-> image
-      zimage/image->output-stream fmt
+      (zimage/image->output-stream fmt)
       response
-      content-type "image/png"))
+      (content-type "image/png"))) 
 
-(def app
-  (-> app-routes
-      wrap-log-request))
+(defprotocol ServerRoutes
+  (add-route! [this route])
+  (get-routes [this]))
+
+(defprotocol ImageDisplay
+  (imshow! [this image fmt image-name]))
+
+(deftype ServerState [routez])
+
+(extend-type ServerState
+  ServerRoutes
+  (add-route! [this route]
+    (swap! (.routez this) conj route)
+    nil)
+  (get-routes [this]
+    (apply ccore/routes @(.routez this))))
+
+(extend-type ServerState
+  ImageDisplay
+  (imshow! [this image fmt image-name]
+    (add-route! this (ccore/GET "/test" [] "jiqj"))))
+
+(defn make-server-state []
+  (->ServerState (atom [])))
+
+(def server-state (make-server-state))
+
+(defn app [x]
+  ((get-routes server-state) x))
+
+;; (def app
+;;   (-> app-routes
+;;       wrap-log-request))
